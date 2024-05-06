759d3ef224264d548c5e05a65a6995cf178ed8a3
Rayane.Silva <161982375+myangel777@users.noreply.github.com>
2024-05-06 15:40:36 -0300

    Add files via upload

---
+++ Lógica de Programação (Em Python)/Aula 01/Exercicio 03.py
@@ -0,0 +1,6 @@
+Nome = input("Nome do aluno: ")
+Nota1 = float(input("Nota atividade 1: "))
+Nota2 = float(input("Nota atividade 2: "))
+Media = (Nota1+Nota2)/2
+print(Nome,Media)
+
---
+++ Lógica de Programação (Em Python)/Aula 01/exercicio 04.py
@@ -0,0 +1,4 @@
+Nome = input("Digite seu nome: ")
+Idade = int(input("Digite sua idade: "))
+Salario = float(input("Digite seu salario: "))
+print(Nome,"tem",Idade,"anos e recebe",Salario )
