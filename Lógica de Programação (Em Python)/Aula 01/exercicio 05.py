b1619da59b3ecd15e095d6cdc5a9e434e721076b
Rayane.Silva <161982375+myangel777@users.noreply.github.com>
2024-05-06 16:24:12 -0300

    Delete Lógica de Programação (Em Python)/Aula 01/exercicio 05.py

--- Lógica de Programação (Em Python)/Aula 01/exercicio 05.py
+++
@@ -1,7 +0,0 @@
-Numero1 = float(input("digite um número"))
-Numero2 = float(input("digite um número"))
-Soma = Numero1 + Numero2
-Subtracao = Numero1 - Numero2
-Divisao = Numero1/Numero2
-Multiplicacao =( Numero1*Numero2)
-print("A soma é",Soma,"A divisão é",Divisao,"A subtração é",Subtracao,"A multiplicação é",Multiplicacao)
