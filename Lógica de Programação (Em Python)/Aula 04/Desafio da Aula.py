5f70622390269e57784680c0e203e0570f95a82b
Rayane.Silva <161982375+RayaneSilva004@users.noreply.github.com>
2024-05-13 15:07:06 -0300

    Update Desafio da Aula.py

--- Lógica de Programação (Em Python)/Aula 04/Desafio da Aula.py
+++ Lógica de Programação (Em Python)/Aula 04/Desafio da Aula.py
@@ -1,7 +1,3 @@
-"""n1 = int(input("Digite um número: "))
-for x in range(1,n1+1):
-    for y in range(1, x+1):
-        print(x,  end=" ")"""
 n1 = int(input("Digite um número: "))
 for x in range(1, n1 + 1):
     for y in range(1, x + 1):
