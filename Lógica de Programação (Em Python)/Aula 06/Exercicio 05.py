5aeaa1b1d8df1d4208fcd10e2df97f2bf966b107
Rayane.Silva <161982375+RayaneSilva004@users.noreply.github.com>
2024-05-13 17:05:58 -0300

    Update Exercicio 05.py

--- Lógica de Programação (Em Python)/Aula 06/Exercicio 05.py
+++ Lógica de Programação (Em Python)/Aula 06/Exercicio 05.py
@@ -1,4 +1,4 @@
-"""num1 = int(input("Digite um número:"))
+num1 = int(input("Digite um número:"))
 num2 = int(input("Digite um número:"))
 num3 = int(input("Digite um número:"))
 if num1 > num2:
@@ -10,10 +10,4 @@ else:
     if num2 > num3:
         print(f"{num2} é o maior número")
     else:
-        print(f"{num3} é maior número")"""
-ref=0
-for x in range(3):
-    n= int(input("Quantas atividades foram realizadas?: "))
-    if n > ref:
-        ref=n
-print(ref)
+        print(f"{num3} é maior número")
