fc33e06e44aad61febb3f66a7001638179fb3c5a
Rayane.Silva <161982375+RayaneSilva004@users.noreply.github.com>
2024-05-13 17:06:17 -0300

    Update Exercicio 06.py

--- Lógica de Programação (Em Python)/Aula 06/Exercicio 06.py
+++ Lógica de Programação (Em Python)/Aula 06/Exercicio 06.py
@@ -1,10 +1,3 @@
-"""n= int(input("Digite um número: "))
-if n==0:
-    print(f"Número Invalido, Número digitado foi {n}")
-elif n%2==0:
-    print("Número par")
-else:
-    print("Número ímpar")"""
 perg= "Sim"
 while perg == "Sim" or perg =="sim":
     n= int(input("Digite um número: "))
