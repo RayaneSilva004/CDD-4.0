439a03cd54afaf08f1900eaa9f9ade494e18e157
Rayane.Silva <161982375+RayaneSilva004@users.noreply.github.com>
2024-05-20 17:04:49 -0300

    Add files via upload

---
+++ Introdução Python/Programação Orientada a Objetos/Herança/Heranca.py
@@ -0,0 +1,21 @@
+class Forma():
+    def __init__(self):
+        self.area = 0
+        self.perimetro = 0
+class Retangulo(Forma):
+    def __init__(self):
+        super().__init__()
+    def calcularArea(self, largura, altura):
+        self.area = largura * altura
+        print(f"A área é {self.area}")
+
+    def calcularPerimetro(self, largura, altura):
+        self.perimetro = 2 * (largura * altura)
+        print(f"O perimetro é {self.perimetro}")
+
+class Triangulo(Forma):
+    def __init__(self):
+        super().__init__()
+    def calcularArea(self, base, altura):
+        self.area = (base * altura)/2
+        print(f"A área do triângulo é {self.area}")
