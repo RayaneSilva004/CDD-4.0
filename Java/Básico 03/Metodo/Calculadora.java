5e81505e840a92f6daa1b16739bebdc8f1d85c3f
Rayane.Silva <161982375+RayaneSilva004@users.noreply.github.com>
2024-07-11 16:44:13 -0300

    Add files via upload

---
+++ Java/Básico 03/Metodo/Calculadora.java
@@ -0,0 +1,11 @@
+package Metodos;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Calcularmetodos C1 = new Calcularmetodos();
+		int A=C1.somar(12,5,5);
+		System.out.println(A);
+	}
+
+}
